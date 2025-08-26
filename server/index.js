a7fd72f0c7a03902c4951853b75dc871a01bfc06
Hasnain Khushi <hasnainkhushii@gmail.com>
2025-08-26 12:21:51 +0500

    connecting to the database

--- server/index.js
+++ server/index.js
@@ -8,6 +8,8 @@ const AuthRouter = require('./Router/authRouter')
 const app = express()
 const PORT = process.env.PORT || 5000;
 
+const MONGO_URL = process.env.MONGO_URL
+
 app.use(cors({ origin: 'http://localhost:5173' }));
 app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -16,6 +18,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/api', AuthRouter)
 
-app.listen(PORT, () => {
+mongoose.connect(MONGO_URL, { dbName: 'Dorooing(LMG)' }).then(app.listen(PORT, () => {
+    console.log('Connected to the database');
     console.log(`Server is running on http://localhost:${PORT}`);
-});
+})).catch((err) => {
+    console.log('Sorry wen could not connect to the database', err.message);
+})
