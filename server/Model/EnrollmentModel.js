7b0f95e3dec5005bb930fa4c047beb2fd2b4df2c
Hasnain Khushi <hasnainkhushii@gmail.com>
2025-09-01 12:01:57 +0500

    adding the enrollment stucture in the beckend to store the data in the database

---
+++ server/Model/EnrollmentModel.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+const enrollmentSchema = new mongoose.Schema({
+    studentId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    courseId: { type: mongoose.Schema.Types.ObjectId, ref: "AddingCourse", required: true },
+    enrolledAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model("Enrollment", enrollmentSchema);
