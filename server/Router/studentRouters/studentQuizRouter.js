d12c773921badbf7cae25e3558cf5de5a2ad1c66
Hasnain Khushi <hasnainkhushii@gmail.com>
2025-09-02 12:48:21 +0500

    fix beckend problem

--- server/Router/studentRouters/studentQuizRouter.js
+++ server/Router/studentRouters/studentQuizRouter.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const studentQuizController = require("../Controller/studentControllers/studentQuizController");
-const { verifyToken } = require("../middleware/authMiddleware");
+const studentQuizController = require("../../Controller/student/studentQuizController.js");
+const { verifyToken } = require("../../middleware/authMiddleware.js");
 
 router.get("/me", verifyToken, studentQuizController.getQuizzesForStudent);
 
